c80014bf64a8c8a6a6ef48ffa4ac00cec9af2e65
nabi-0 <62216642+nabi-0@users.noreply.github.com>
2020-07-13 23:03:36 -0500

    creating schemas and seeds files

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,25 @@
---WRITE SQL QUERIES TO THIS FILE THAT DO THE FOLLOWING:
+/*
+To run this file, we do the following in our Terminal:
+
+1. Go to the directory of this sql file.
+
+2. Get into our mysql console.
+
+3. Run "source schema.sql"
+*/
+-- Drops the burgers_db if it already exists --
+DROP DATABASE IF EXISTS burgers_db;
 
 --Create the `burgers_db`.
+CREATE DATABASE burgers_db;
 
---Switch to or use the `burgers_db`.
+--Switch to or use the `burgers_db`
+USE burgers_db;
 
---Create a `burgers` table with these fields:
-
---**id**: an auto incrementing int that serves as the primary key.
---**burger_name**: a string.
---**devoured**: a boolean.
+--Create a `burgers` table: id, name devoured
+CREATE TABLE burgers (
+  id INT NOT NULL AUTO_INCREMENT,
+  burger_name VARCHAR(255) NOT NULL,
+  devoured BOOLEAN,
+  PRIMARY KEY (id)
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,10 @@
 --WRITE INSERT QUERIES TO PUPULATE THE `BURGERS` TABLE (THREE ENTRIES)
+
+INSERT INTO burgers (burgers_name, true)
+VALUE ("DoubleCheese Burger");
+
+INSERT INTO burgers (burgers_name, true)
+VALUE ("Tuna Burger");
+
+INSERT INTO burgers (burgers_name, true)
+VALUE ("Pizza Burger");
