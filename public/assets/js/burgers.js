95fa558ec0c7da61ee5b6905ab3cb72d6af9d848
nabi-0 <62216642+nabi-0@users.noreply.github.com>
2020-07-19 03:49:27 -0500

    seeedss

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,6 +1,6 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-    $(".change-devoured").on("click", function(event) {
+    $(".change-devour").on("click", function(event) {
       var id = $(this).data("id");
       var newDev = $(this).data("newDev");
   
